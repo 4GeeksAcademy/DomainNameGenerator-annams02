1b648fbe2bc001cba300f1b9986fcf5915173352
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-03-11 17:10:56 +0000

    segunda manera de hacer for in

--- src/app.js
+++ src/app.js
@@ -6,16 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [".com", ".es", ".us"];
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".es", ".us"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < extension.length; l++) {
-          console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+  for (let pronoun in pronouns) {
+    for (let adj in adjs) {
+      for (let noun in nouns) {
+        for (let extension in extensions) {
+          console.log(
+            pronouns[pronoun] + adjs[adj] + nouns[noun] + extensions[extension]
+          );
         }
       }
     }
